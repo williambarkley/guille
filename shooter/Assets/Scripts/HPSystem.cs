83af4d7d1126d632d6c69b31e88ad796908a7e20
Aihotz <aihotz_arruti_aldama@outlook.es>
2020-10-07 14:08:51 +0300

    Player movement

---
+++ shooter/Assets/Scripts/HPSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HPSystem : MonoBehaviour
+{
+    const int MAX_HP = 3;
+    int HP;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP = MAX_HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerDead())
+            Debug.Log("Game Over");
+        //game end
+
+        if (HP > MAX_HP)
+            HP = MAX_HP;
+    }
+
+    public void removeHP()
+    {
+        HP--;
+    }
+
+    public void addHP()
+    {
+        HP++;
+    }
+
+    bool playerDead()
+    {
+        if (HP <= 0)
+            return true;
+        else
+            return false;
+    }
+}
---
+++ shooter/Assets/Scripts/Player_movement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_movement : MonoBehaviour
+{
+    const int MAX_X = 3;
+    const int MIN_X = -3;
+    const int MAX_Y = 3;
+    const int MIN_Y = -3;
+
+
+    public Rigidbody2D rb;
+    public HPSystem HP;
+
+    Vector2 movement;
+    public float speed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        //Player wont exit the limits
+        inLimits();
+
+        if (Input.GetKeyDown("space"))
+            HP.removeHP();
+        if (Input.GetKeyDown("j"))
+            HP.addHP();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    void inLimits()
+    {
+        if (rb.position.x > MAX_X)
+            rb.position = new Vector2(MAX_X, rb.position.y);
+        if (rb.position.x < MIN_X)
+            rb.position = new Vector2(MIN_X, rb.position.y);
+
+        if (rb.position.y > MAX_Y)
+            rb.position = new Vector2(rb.position.x, MAX_Y);
+        if (rb.position.y < MIN_Y)
+            rb.position = new Vector2(rb.position.x, MIN_Y);
+    }
+}
